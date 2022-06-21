3d3ed4c423d35f1a9a1171b4e38a5731a232f29b
agchitre <agchitre@amazon.com>
2022-06-21 12:35:48 -0700

    size changes

--- Welcome.js
+++ Welcome.js
@@ -1,188 +1,85 @@
-
 var Welcome = new Phaser.Class({
     Extends: Phaser.Scene,
     initialize: function() {
         Phaser.Scene.call(this, { "key": "Welcome" });
     },
-    init: function() {},
+    init: function() {
+       
+    },
     preload: function() {
-                //this.load.audio('gunshot','assets/gunshot.mp3');
-                this.load.image('play', 'assets/play.png');
-                this.load.audio('gunshotlong','assets/gunshotlong.mp3');
-                this.load.audio('background','assets/backgroundMusic.mp3');
-                this.load.bitmapFont('desyrel', 'assets/fonts/bitmap/desyrel.png', 'assets/fonts/bitmap/desyrel.xml');
-                this.load.image('sky', 'assets/space3.png');
-                this.load.image('logo', 'assets/phaser3-logo.png');
-                this.load.image('red', 'assets/red.png');
-                this.load.image('westback', 'assets/Desertbackground.png');
-                this.load.image('ground', 'assets/platform.png');
-                this.load.image('cactus', 'assets/cactus.png');
-                this.load.image('box', 'assets/box.png');
-                this.load.image('ammo', 'assets/ammo.png');
-                this.load.image('barrels', 'assets/barrels.png');
-                this.load.image('Explosivs', 'assets/Explosivs.png');
-                this.load.image('barrels2', 'assets/Barrels_2.png');
-                
-        
-                
-                
-                this.load.path = 'assets/SherifShoot/';
-        
-                this.load.image('1', '1.png');
-                this.load.image('2', '2.png');
-                this.load.image('3', '3.png');
-                this.load.image('4', '4.png');
-                this.load.image('5', '5.png');
-                this.load.image('6', '6.png');
-                this.load.image('7', '7.png');
-                this.load.image('8', '8.png');
-                this.load.image('9', '9.png');
-                this.load.image('10', '10.png');
-                this.load.image('11', '11.png');
-                this.load.image('12', '12.png');
-                this.load.image('13', '13.png');
-                this.load.image('14', '14.png');
-                this.load.image('15', '15.png');
-                this.load.image('16', '16.png');
-                this.load.image('17', '17.png');
-                this.load.image('18', '18.png');
-                this.load.image('19', '19.png');
-        
-                this.load.path = 'assets/Sherifdead/';
-        
-                this.load.image('21', '1.png');
-                this.load.image('22', '2.png');
-                this.load.image('23', '3.png');
-                this.load.image('24', '4.png');
-                this.load.image('25', '5.png');
-                this.load.image('26', '6.png');
-                this.load.image('27', '7.png');
-                this.load.image('28', '8.png');
-                this.load.image('29', '9.png');
-                this.load.image('30', '10.png');
-                this.load.image('31', '11.png');
-
-                
+        this.load.image('westbackGame', 'assets/background.jpeg');
+        this.load.image('play', 'assets/play.png');
+        this.load.image('fence', 'assets/fence.png');
+        this.load.image('box', 'assets/box.png');
+        this.load.audio('gunshotlong','assets/gunshotlong.mp3');
+        this.load.audio('background','assets/backgroundMusic.mp3');
+        this.load.bitmapFont('desyrel', 'assets/fonts/bitmap/desyrel.png', 'assets/fonts/bitmap/desyrel.xml');
     },
     create: function() {
-        
 
-        this.music =  this.sound.add('gunshotlong', {
+
+        /*this.music =  this.sound.add('gunshotlong', {
             volume: 0.2,
             loop: false
             });
-            this.music.play();
+            this.music.play();*/
     
             this.music =  this.sound.add('background', {
             volume: 0.2,
             loop: true
             });
             this.music.play();
-    
-            this.add.image(400, 300, 'westback');
-            ammo = this.add.image(150,510,'ammo');
-            barrels = this.add.image(250,510,'barrels');
-            explosivs = this.add.image(310,510,'Explosivs');
-            //barrels2 = this.add.image(300,510,'barrels2');
-          //  this.add.image(380,400,'box');
-            this.add.image(850,510,'ammo');
-            this.add.image(820,555,'ammo');
-            cactus = this.add.image(850,510,'cactus'); 
-    
-            //animation for Sherif1 dead/////
-            this.anims.create({
-                key: 'dead',
-                frames: [
-                    { key: '21' },
-                    { key: '22' },
-                    { key: '23' },
-                    { key: '24' },
-                    { key: '25' },
-                    { key: '26' },
-                    { key: '27' },
-                    { key: '28' },
-                    { key: '29' },
-                    { key: '30' },
-                    { key: '31', duration: 50 }
-                ],
-                frameRate: 5
-            });
-    
-            GG = this.add.sprite(250, 350, '1')
-                .play('dead');
-                GG.flipX=true;
-    
-    
-            //animation for Sheriff2 shooter/////
-            this.anims.create({
-                key: 'snooze',
-                frames: [
-                    { key: '1' },
-                    { key: '2' },
-                    { key: '3' },
-                    { key: '4' },
-                    { key: '5' },
-                    { key: '6' },
-                    { key: '7' },
-                    { key: '8' },
-                    { key: '9' },
-                    { key: '10' },
-                    { key: '11' },
-                    { key: '12' },
-                    { key: '13' },
-                    { key: '14' },
-                    { key: '15' },
-                    { key: '16' },
-                    { key: '17' },
-                    { key: '18' },
-                    { key: '19', duration: 50 }
-                ],
-                frameRate: 18,
-                repeat: -1
-            });
-    
-            this.add.sprite(700, 360, '1')
-                .play('snooze');
-    
-    
-            // text
-    
-            var container = this.add.container(400, 100);
-    
-            //const text = this.add.bitmapText(100, 300, 'desyrel', 'CowBoys', 38)
-              //  .setOrigin(0.5, 0.5);
-    
-    
-    
-    
-            var text = this.add.text(0, 0, 'CowBoys');
-            text.font = "Press Start 2P";
-            text.setColor("#FFD700");
-            text.fontStyle = "strong";  
-            text.setOrigin(0.5, 0.5);
-            container.add(text);
-            container.setScale(3);
-    
-            this.tweens.add({
-                targets: container,
-                x: container.x + 100,
-                ease: 'Power1',
-                duration: 5000,
-                delay: 400,
-                yoyo: true,
-                repeat: -1
-            });
-    
-            back_text = this.add.text(10, 10, 'Try, Alexa play a game!', { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' });
-            back_text.setColor("#FFFAFA");
-            //button start
-            const gamePlayButton = this.add.image(520, 510, 'play');
-            gamePlayButton.setScale(.15);
-            gamePlayButton.setInteractive();
-            gamePlayButton.on('pointerdown', () => {
-                this.scene.start("GamePlay",{"message": "Game Play"});
-            });
 
+
+       backgroundGame=  this.add.image(400, 300, 'westbackGame');
+        var text = this.add.text(
+            640, 
+            360, 
+            this.message, 
+            {
+                fontSize: 50,
+                color: "#000000",
+                fontStyle: "bold"
+            }
+        ).setOrigin(0.5);
+
+        var container = this.add.container(400, 100);
+        var text = this.add.text(0, 0, 
+            'Cowboys & Cowboys', 
+            {
+            fontSize: 20,
+            color: "#000000",
+            fontStyle: "bold"
+            }
+            ).setOrigin(0.5);
+  //      text.font = "Press Start 2P";
+ //       text.setColor("#FFD700");
+ //       text.fontStyle = "strong";  
+//        text.setOrigin(0.5, 0.5);
+        container.add(text);
+        container.setScale(3);
+
+        this.tweens.add({
+            targets: container,
+            x: container.x + 100,
+            ease: 'Power1',
+            duration: 5000,
+            delay: 400,
+            yoyo: true,
+            repeat: -1
+        });
+
+        back_text = this.add.text(10, 10, 'Try, Alexa play a game!', { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' });
+        back_text.setColor("#FFFAFA");
+        fence = this.add.image(520,510,'fence');
+       
+        //button start
+        const gamePlayButton = this.add.image(520, 510, 'play');
+        gamePlayButton.setScale(.15);
+        gamePlayButton.setInteractive();
+        gamePlayButton.on('pointerdown', () => {
+            this.scene.start("GamePlay",{"message": "Game Play"});
+        });
     },
     update: function() {}
 });
